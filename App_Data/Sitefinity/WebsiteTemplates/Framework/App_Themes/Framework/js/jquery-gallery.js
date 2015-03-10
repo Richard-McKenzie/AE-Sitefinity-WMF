ad252f73c19440d3bf5f1159435f08024ee65245
Matt Norton <mcleodwebdev@gmail.com>
2015-03-10 16:51:54 -0400

    Adding Photo Gallery 3/10

---
+++ App_Data/Sitefinity/WebsiteTemplates/Framework/App_Themes/Framework/js/jquery-gallery.js
@@ -0,0 +1,10 @@
+/* ==========================================================================
+ * JQUERY Specific to Gallery (transfer over to jquery functions if you are going to use them)
+ * ========================================================================== */
+
+jQuery(function($) {
+// Option 1 - Single Slider
+	$('.gallery-list').slick({
+		  infinite: true,
+	});
+});
