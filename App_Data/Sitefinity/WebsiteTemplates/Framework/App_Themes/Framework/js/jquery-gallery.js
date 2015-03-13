d754e62d3bda95d2839371cd0d19f5ef7903b828
Matt Norton <mcleodwebdev@gmail.com>
2015-03-13 08:29:48 -0400

    2015-03-12

--- App_Data/Sitefinity/WebsiteTemplates/Framework/App_Themes/Framework/js/jquery-gallery.js
+++ App_Data/Sitefinity/WebsiteTemplates/Framework/App_Themes/Framework/js/jquery-gallery.js
@@ -3,8 +3,78 @@
  * ========================================================================== */
 
 jQuery(function($) {
-// Option 1 - Single Slider
-	$('.gallery-list').slick({
+// Single Slider
+	$('.slider').slick({
 		  infinite: true,
+		  dots: true,
+	});
+
+// Slider with Thumbnails Horizontal
+	 $('.gallery-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  asNavFor: '.gallery-nav',
+	  arrows:false,
+	});
+	$('.gallery-nav').slick({
+	  slidesToShow: 6,
+	  slidesToScroll: 1,
+	  asNavFor: '.gallery-for',
+	  lazyLoad: 'ondemand',
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 5,
+	         centerPadding: '0',
+	         
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 3,
+	        centerPadding: '0',
+	      }
+	    }
+	  ]
+	});
+
+// Slider with Vertical Thumbnails
+	 $('.vert-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  asNavFor: '.vert-nav',
+	  arrows:false,
+	});
+	$('.vert-nav').slick({
+	  slidesToShow: 6,
+	  slidesToScroll: 1,
+	  asNavFor: '.vert-for',
+	  lazyLoad: 'ondemand',
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 5,
+	         centerPadding: '0',
+	         
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 3,
+	        centerPadding: '0',
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
 	});
 });
